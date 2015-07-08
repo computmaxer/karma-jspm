71a111450e2490bf1b691188dfcf5f2c872b2969
omerts <omerts3@gmail.com>
2015-07-08 15:27:12 +0300

    removed redundant space

--- src/init.js
+++ src/init.js
@@ -96,7 +96,7 @@ module.exports = function(files, basePath, jspm, client) {
   
   files.unshift(createPattern(__dirname + '/adapter.js'));
   files.unshift(createPattern(getLoaderPath('system-polyfills.src')));
-  files.unshift(createPattern(getLoaderPath('system.src')));  
+  files.unshift(createPattern(getLoaderPath('system.src')));
 
   // Loop through all of jspm.load_files and do two things
   // 1. Add all the files as "served" files to the files array
