8a58bc1b188747fc915fdec652c8266a359d8c49
Sven Reglitzki <sven.reglitzki@flavia-it.de>
2016-05-12 22:06:29 +0200

    Fix merge error and tests

--- src/init.js
+++ src/init.js
@@ -126,7 +126,7 @@ module.exports = function(files, basePath, jspm, client) {
 
   // Allow Karma to serve all files within jspm_packages.
   // This allows jspm/SystemJS to load them
-  var jspmPattern = createServedPattern(packagesPath + '**/*', jspm.cachePackages !== true);
+  var jspmPattern = createServedPattern(packagesPath + '**/*', {nocache: jspm.cachePackages !== true});
   jspmPattern.watched = false;
   files.push(jspmPattern);
 };
